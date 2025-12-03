2a8d8750bd720ddb7de8eed723abca1e2f5df91d
Zeno Maccari <zeno.maccari@studio.unibo.it>
2025-12-03 16:22:07 +0100

    Implement basic match scraping

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ cookies = {'buckler_id' : BUCKLER_ID}
 headers = {'User-Agent': USER_AGENT}
 
 
+
+
 contents = requests.get("https://www.streetfighter.com/6/buckler/it/profile/"+USER_ID+"/battlelog/rank#profile_na",headers=headers,cookies=cookies)
 print(contents.status_code)
 if(contents.status_code == 200):
@@ -24,6 +26,37 @@ if(contents.status_code == 200):
     battlelog = parsed["props"]["pageProps"]["replay_list"]
     open("log.json", "w", encoding="utf-8").write(json.dumps(battlelog, ensure_ascii=False, indent=2))
     open("index.html", "w").write(str(soup.body.prettify()))
+    parsedMatches = []
+    for match in battlelog:
+        if (match["player1_info"]["player"]["short_id"]== USER_ID):
+            parsedMatch={
+                "id": match["replay_id"],
+                "me":{
+                    "character": match["player1_info"]["character_name"],
+                }
+                ,
+                "opponent":{
+                    "character":match["player2_info"]["character_name"],
+                }
+
+            }
+        else:
+            parsedMatch={
+                "id": match["replay_id"],
+                "me":{
+                    "character": match["player2_info"]["character_name"],
+                }
+                ,
+                "opponent":{
+                    "character":match["player1_info"]["character_name"],
+                }
+
+            }
+        parsedMatches.append(parsedMatch)
+    
+    open("cleanedlog.json", "w", encoding="utf-8").write(json.dumps(parsedMatches, ensure_ascii=False, indent=2))
     
     
+    
+
     
