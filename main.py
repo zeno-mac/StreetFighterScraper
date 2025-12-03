e83f5929777e1447882c296bc60b9716aa29bda4
Zeno Maccari <zeno.maccari@studio.unibo.it>
2025-12-03 20:43:52 +0100

    Add multiple page scraping and more in depth scraping

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import json
 from dotenv import load_dotenv
 import os
+from datetime import datetime
+
 
 load_dotenv()
 
@@ -13,48 +15,55 @@ cookies = {'buckler_id' : BUCKLER_ID}
 headers = {'User-Agent': USER_AGENT}
 
 
+def fillPlayerInfo(player):
+    info = {
+        "character":player["character_name"],
+        "MR":player["master_rating"],
+        "input_type":transleInput(player["battle_input_type_name"]),
+        }
+    return info
 
-
-contents = requests.get("https://www.streetfighter.com/6/buckler/it/profile/"+USER_ID+"/battlelog/rank#profile_na",headers=headers,cookies=cookies)
-print(contents.status_code)
-if(contents.status_code == 200):
-    soup = BeautifulSoup(contents.content, "html.parser")
-    logClass = "battle_data_battlelog__list__JNDjG"
-    data = soup.find("script" , id = "__NEXT_DATA__")
-    parsed = json.loads(data.string)
-    open("next_data.json", "w", encoding="utf-8").write(json.dumps(parsed, ensure_ascii=False, indent=2))
-    battlelog = parsed["props"]["pageProps"]["replay_list"]
-    open("log.json", "w", encoding="utf-8").write(json.dumps(battlelog, ensure_ascii=False, indent=2))
-    open("index.html", "w").write(str(soup.body.prettify()))
-    parsedMatches = []
-    for match in battlelog:
-        if (match["player1_info"]["player"]["short_id"]== USER_ID):
-            parsedMatch={
-                "id": match["replay_id"],
-                "me":{
-                    "character": match["player1_info"]["character_name"],
-                }
-                ,
-                "opponent":{
-                    "character":match["player2_info"]["character_name"],
-                }
-
-            }
-        else:
-            parsedMatch={
-                "id": match["replay_id"],
-                "me":{
-                    "character": match["player2_info"]["character_name"],
-                }
-                ,
-                "opponent":{
-                    "character":match["player1_info"]["character_name"],
-                }
-
-            }
-        parsedMatches.append(parsedMatch)
+def transleInput(name):
+    classic = "[t]クラシック"
+    modern = "[t]モダン"
+    if name == classic:
+        return "classic"
+    else:
+        return "modern"
     
-    open("cleanedlog.json", "w", encoding="utf-8").write(json.dumps(parsedMatches, ensure_ascii=False, indent=2))
+parsedMatches = []
+for i in range(10):
+    contents = requests.get("https://www.streetfighter.com/6/buckler/it/profile/"+USER_ID+"/battlelog/rank?page="+str(i+1),headers=headers,cookies=cookies)
+    print(contents.status_code)
+    if(contents.status_code == 200):
+        soup = BeautifulSoup(contents.content, "html.parser")
+        logClass = "battle_data_battlelog__list__JNDjG"
+        next_data = soup.find("script" , id = "__NEXT_DATA__")
+        parsed = json.loads(next_data.string)
+        battlelog = parsed["props"]["pageProps"]["replay_list"]
+ 
+        #open("next_data.json", "w", encoding="utf-8").write(json.dumps(parsed, ensure_ascii=False, indent=2)) 
+        #open("log.json", "w", encoding="utf-8").write(json.dumps(battlelog, ensure_ascii=False, indent=2))
+        ##open("index.html", "w", encoding="utf-8").write(str(soup.body.prettify()))
+        
+        for match in battlelog:
+            if (str(match["player1_info"]["player"]["short_id"])== USER_ID):
+                my_info  = match["player1_info"]
+                opponent_info = match["player2_info"]
+            else:
+                my_info = match["player2_info"]
+                opponent_info = match["player1_info"]
+            
+            parsedMatch={
+                "id": match["replay_id"],
+                "uploaded_at" :match["uploaded_at"],
+                "date": datetime.fromtimestamp(match["uploaded_at"]).strftime("%Y-%m-%d %H:%M:%S"),
+                "me": fillPlayerInfo(my_info),
+                "opponent":fillPlayerInfo(opponent_info),
+            }
+            parsedMatches.append(parsedMatch)
+parsedMatches.reverse()
+open("cleanedlog.json", "w", encoding="utf-8").write(json.dumps(parsedMatches, ensure_ascii=False, indent=2))
     
     
     
