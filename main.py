2e647d0fbaa86007df1d70587b5015126b014a3f
Zeno Maccari <zeno.maccari@studio.unibo.it>
2025-12-18 11:52:25 +0100

    Improve GAS Log

--- main.py
+++ main.py
@@ -164,7 +164,7 @@ def send_gas_request(cfg: Config, data):
         if "GAS" in r.text:
             raise KeyError(f"Script returned an error: {r.text}")
         else:
-            logger.info(f"Successful: {r.text}")
+            logger.info(f"Successful Google App Script request: {r.text}")
     else:
         raise KeyError(f"Error occurred during Google App Scripts request: status code {r.status_code}\nError: {r.text}")
       
